a38ec1dd7b3f31edd426cbde83122625b63a1305
Alex Mierzejewski <not set>
2021-02-22 20:58:35 -0500

    Initial Commit

---
+++ flixster/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  flixster
+//
+//  Created by Alex Mierzejewski on 2/22/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
